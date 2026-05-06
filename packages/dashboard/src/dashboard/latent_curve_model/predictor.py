2281fba3c0332cdd9244e26bae6137922e77e726
Jakub Zika <kuba.zika@email.cz>
2026-05-06 20:24:20 +0200

    configure latentcurvemodel db connection string

--- packages/dashboard/src/dashboard/latent_curve_model/predictor.py
+++ packages/dashboard/src/dashboard/latent_curve_model/predictor.py
@@ -1,3 +1,10 @@
-from latentcurvemodel import ChargingProfilePredictor
+from dashboard.settings import settings
+from latentcurvemodel import (
+    ChargingProfilePredictor,
+    settings as latentcurvemodel_settings,
+)
 
+latentcurvemodel_settings.postgres_connection_string = (
+    settings.POSTGRES_CONNECTION_STRING
+)
 predictor = ChargingProfilePredictor()
